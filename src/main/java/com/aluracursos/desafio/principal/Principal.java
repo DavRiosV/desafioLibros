b49f1c7614e4242f830b2922371bbbaf557c2e14
David Rios <david.r.v@hotmail.com>
2025-07-08 00:27:47 -0400

    Primer commit implementando api de libros

---
+++ src/main/java/com/aluracursos/desafio/principal/Principal.java
@@ -0,0 +1,29 @@
+package com.aluracursos.desafio.principal;
+
+import com.aluracursos.desafio.model.Datos;
+import com.aluracursos.desafio.model.DatosLibros;
+import com.aluracursos.desafio.service.ConsumoApi;
+import com.aluracursos.desafio.service.ConvierteDatos;
+
+import java.util.Comparator;
+
+public class Principal {
+    private  static final String URL_BASE = "https://gutendex.com/books/";
+    private ConsumoApi consumoApi = new ConsumoApi();
+    private ConvierteDatos converson = new ConvierteDatos();
+
+    public void muestraElMenu(){
+        var json = consumoApi.obtenerDatos(URL_BASE);
+        System.out.println(json);
+        var datos = converson.obtenerDatos(json, Datos.class);
+        System.out.println(datos);
+
+        //Top 10 libros más descargados
+        System.out.println("Top 10 libros más descargados.");
+        datos.resultados().stream()
+                .sorted(Comparator.comparing(DatosLibros::numeroDeDescargas).reversed())
+                .limit(10)
+                .map(l ->l.titulo().toUpperCase())
+                .forEach(System.out::println);
+    }
+}
